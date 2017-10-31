7053065c114b3f6f3357dfd287d79bc49f3f652e
Bryan Hughes <bryan.hughes@spacetimeinsight.com>
2017-10-31 13:37:17 -0700

    WIP

--- scripts/iot_demo.py
+++ scripts/iot_demo.py
@@ -13,17 +13,12 @@ if __name__=="__main__":
     rgb_led = grove_led.ChainableLED(grove_led.CLK_PIN, grove_led.DATA_PIN, grove_led.NUMBER_OF_LEDS)
 
     while True:
-        # GPIO.output(led,GPIO.HIGH)
-        # time.sleep(1)
-        # GPIO.output(led,GPIO.LOW)
-        # time.sleep(1)
-        # print 'led'
         humidity, temperature = grove_dht.read()
         
         grove_oled.oled_setTextXY(0,0)
-        grove_oled.oled_putString('Temp:{0:0.1f} C'.format(temperature))
+        grove_oled.oled_putString('Temp:{0:0.1f}C'.format(temperature))
         grove_oled.oled_setTextXY(1,0)
-        grove_oled.oled_putString('Humidity={0:0.1f} %'.format(humidity))
+        grove_oled.oled_putString('Hum: {0:0.1f}%'.format(humidity))
         grove_oled.oled_setTextXY(5,0)
         grove_oled.oled_putString("SpaceTime")
 
